26d86ba4ae89a4b140905f524faf4d452ed5ddb3
khufiyajasuse <140427665+khufiyajasuse@users.noreply.github.com>
2025-02-15 22:38:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
 
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-WEBSITE_URL = environ.get("WEBSITE_URL", "https://dramebaazbatman.blogspot.com/2025/02/telegram.html") # For More Information Check Video On Yt - @Tech_VJ
+WEBSITE_URL = environ.get("WEBSITE_URL", "https://hareemrashid.blogspot.com/2024/07/telegram-hareemrashid.html") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
 STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
